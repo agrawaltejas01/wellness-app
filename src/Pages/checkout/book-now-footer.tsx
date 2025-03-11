256084875141d38a2c806c47d989c9d6f853457c
Vishal <vishaldandge1@gmail.com>
2025-03-11 09:14:41 +0530

    Dummy commit for deployment

--- src/Pages/checkout/book-now-footer.tsx
+++ src/Pages/checkout/book-now-footer.tsx
@@ -90,7 +90,6 @@ function createOrderPayload(props: IBookNowFooter, userDetails: IUser) {
   Mixpanel.track("pay_now_button_clicked_on_checkout_page", {
     ...payload,
   });
-
   return payload;
 }
 
