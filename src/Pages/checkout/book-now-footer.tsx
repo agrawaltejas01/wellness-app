05ee42501a9ecf0737c7280fb02c8e1d6398c76f
Vishal <vishaldandge1@gmail.com>
2025-03-12 18:05:21 +0530

    Changes for web intent

--- src/Pages/checkout/book-now-footer.tsx
+++ src/Pages/checkout/book-now-footer.tsx
@@ -138,10 +138,9 @@ async function displayRazorpay(
   let shouldEnableWebViewIntent = false;
 
   if (window.platformInfo?.platform === "ios") {
-    shouldEnableWebViewIntent = isVersionGreaterOrEqual(
-      window.platformInfo?.version || "",
-      "1.2.0",
-    );
+    shouldEnableWebViewIntent = false;
+  } else if (window.platformInfo?.platform === "android") {
+    shouldEnableWebViewIntent = true;
   }
 
   const options = {
