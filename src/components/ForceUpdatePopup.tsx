d4c101353c90a9aa7e1dcb0245973cea25152dc4
pratik-thorat-github <pratik.thorat01@gmail.com>
2025-04-09 10:01:30 +0530

    added mixpanel event for force update button

--- src/components/ForceUpdatePopup.tsx
+++ src/components/ForceUpdatePopup.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './ForceUpdatePopup.css'; // Assuming you'll add some styles
+import { Mixpanel } from '../mixpanel/init';
 
 const ForceUpdatePopup = () => {
   const handleUpdateClick = () => {
     // Replace with your actual App Store URL
     // const appStoreUrl = 'itms-apps://apps.apple.com/in/app/zenfitx/id6736351969';
+    Mixpanel.track("force_update_clicked");
     const appStoreUrl = 'https://apps.apple.com/in/app/zenfitx/id6736351969';
     window.location.href = appStoreUrl;
   };
