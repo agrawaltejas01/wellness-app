961b831359fd79387932cf05055d9e7a401864d1
pratik-thorat-github <pratik.thorat01@gmail.com>
2025-04-18 09:30:44 +0530

    redirecting to app if zenfitx links opened on mobile web broswer

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,8 @@ import reportWebVitals from "./reportWebVitals";
 
 function checkAndRedirect() {
   const currentUrl = window.location.href;
-  const isGymUrl = /^https?:\/\/zenfitx\.in\/gym\/(30)/.test(currentUrl);
+  // const isGymUrl = /^https?:\/\/zenfitx\.in\/gym\/(30)/.test(currentUrl);
+  const isGymUrl = true;
 
   // Check if we're in React Native WebView
   const isInWebView = Boolean(
