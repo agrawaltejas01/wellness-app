94246bb45b8ce4dc2d87b211ad458ad9bf2dc1a7
pratik-thorat-github <pratik.thorat01@gmail.com>
2025-05-01 20:33:24 +0530

    commenting to run on mobile browser

--- src/index.tsx
+++ src/index.tsx
@@ -80,9 +80,9 @@ function mobileRedirect() {
 }
 
 // Run immediately before React loads
-if (typeof window !== 'undefined') {
-  mobileRedirect();
-}
+// if (typeof window !== 'undefined') {
+//   mobileRedirect();
+// }
 
 
 // function checkAndRedirect() {
