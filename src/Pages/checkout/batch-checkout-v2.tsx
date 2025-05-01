10c1ae18dddcdf13d9e2593721cb2dcc859aa4b2
pratik-thorat-github <pratik.thorat01@gmail.com>
2025-05-02 00:29:08 +0530

    allowing scrolling image

--- src/Pages/checkout/batch-checkout-v2.tsx
+++ src/Pages/checkout/batch-checkout-v2.tsx
@@ -124,7 +124,7 @@ const BatchCheckoutV2: React.FC<IClassCheckout> = () => {
 
   return (
     <div className="flex flex-col">
-        <div className="flex flex-col fixed top-0 left-0 right-0 z-10 bg-gradient-to-r from-black to-transparent bg-cover bg-center"
+        <div className="flex flex-col bg-gradient-to-r from-black to-transparent bg-cover bg-center"
              style={{ backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0)), url(${require('../../images/utils/pickleball.png')})` }}>
           <div className="flex flex-row justify-between px-4 py-4">
             <BackButton onClick={() => navigateToHome()} />
