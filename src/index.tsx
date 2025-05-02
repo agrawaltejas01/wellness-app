85c018fa0600616b3d6cf27aecae9fcf2874a70c
pratik-thorat-github <pratik.thorat01@gmail.com>
2025-05-02 17:37:11 +0530

    app redirection enabled

--- src/index.tsx
+++ src/index.tsx
@@ -85,41 +85,41 @@ function mobileRedirect() {
 // }
 
 
-// function checkAndRedirect() {
-//   const currentUrl = window.location.href;
-//   // const isGymUrl = /^https?:\/\/zenfitx\.in\/gym\/(30)/.test(currentUrl);
-//   const isGymUrl = true;
+function checkAndRedirect() {
+  const currentUrl = window.location.href;
+  // const isGymUrl = /^https?:\/\/zenfitx\.in\/gym\/(30)/.test(currentUrl);
+  const isGymUrl = true;
 
-//   // Check if we're in React Native WebView
-//   const isInWebView = Boolean(
-//     (window as any).ReactNativeWebView ||
-//       navigator.userAgent.toLowerCase().includes("wv") ||
-//       navigator.userAgent.toLowerCase().includes("webview") ||
-//       navigator.userAgent.toLowerCase().includes("react-native"),
-//   );
+  // Check if we're in React Native WebView
+  const isInWebView = Boolean(
+    (window as any).ReactNativeWebView ||
+      navigator.userAgent.toLowerCase().includes("wv") ||
+      navigator.userAgent.toLowerCase().includes("webview") ||
+      navigator.userAgent.toLowerCase().includes("react-native"),
+  );
 
-//   // Only redirect if it's a gym URL and we're not in WebView
-//   if (isGymUrl && !isInWebView) {
-//     // Clear the page content first
-//     document.body.innerHTML = "";
-//     document.body.style.backgroundColor = "#FFFFFF";
+  // Only redirect if it's a gym URL and we're not in WebView
+  if (isGymUrl && !isInWebView) {
+    // Clear the page content first
+    document.body.innerHTML = "";
+    document.body.style.backgroundColor = "#FFFFFF";
 
-//     // Small delay before redirect to ensure blank screen is shown
-//     setTimeout(() => {
-//       const userAgent = navigator.userAgent.toLowerCase();
+    // Small delay before redirect to ensure blank screen is shown
+    setTimeout(() => {
+      const userAgent = navigator.userAgent.toLowerCase();
 
-//       if (/iphone|ipad|ipod/.test(userAgent)) {
-//         window.location.href = "https://apps.apple.com/app/id6736351969";
-//       } else if (/android/.test(userAgent)) {
-//         window.location.href =
-//           "https://play.google.com/store/apps/details?id=com.zenfitx.zenfitxapp";
-//       }
-//     }, 200);
-//   }
-// }
+      if (/iphone|ipad|ipod/.test(userAgent)) {
+        window.location.href = "https://apps.apple.com/app/id6736351969";
+      } else if (/android/.test(userAgent)) {
+        window.location.href =
+          "https://play.google.com/store/apps/details?id=com.zenfitx.zenfitxapp";
+      }
+    }, 200);
+  }
+}
 
 // // Run the check when the page loads
-// window.addEventListener("load", checkAndRedirect);
+window.addEventListener("load", checkAndRedirect);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
