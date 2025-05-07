eb218f7d822ec3d64759a133a63455a3961c216b
pratik-thorat-github <pratik.thorat01@gmail.com>
2025-05-07 08:48:00 +0530

    reverted changes

--- src/Pages/checkout/checkout-v3.tsx
+++ src/Pages/checkout/checkout-v3.tsx
@@ -306,7 +306,7 @@ const CheckoutV3: React.FC<IClassCheckout> = ({skillLevel}) => {
           } else {
             finalPrice = Math.floor(finalPrice);
           }
-          let newTotalAmount = price * noOfGuests;
+          let newTotalAmount = finalPrice;
           let discount = price * noOfGuests - finalPrice;
     
           setTotalAmount(newTotalAmount);
