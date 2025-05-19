97abb28c2d28746f26229ef125282d3482069812
pratik-thorat-github <pratik.thorat01@gmail.com>
2025-05-19 11:37:05 +0530

    added grid on home page

--- src/Pages/home/centers-around-you.tsx
+++ src/Pages/home/centers-around-you.tsx
@@ -207,9 +207,11 @@ function getListOfCenters(
   return (
     <Flex flex={1} vertical justify="space-evenly" style={{ width: "100%" }}>
       {/* {generateCards} */}
-      {gymCardsData.map((gymCard) => {
-        return cardWidget(gymCard, isFromApp, pastAppBookings);
-      })}
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+        {gymCardsData.map((gymCard) => {
+          return cardWidget(gymCard, isFromApp, pastAppBookings);
+        })}
+      </div>
     </Flex>
   );
 }
