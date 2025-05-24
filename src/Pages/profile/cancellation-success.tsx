f9aebcb62923731735daa93cc18a06e175b00ef2
pratik-thorat-github <pratik.thorat01@gmail.com>
2025-05-24 10:58:10 +0530

    redirecting after cancellation

--- src/Pages/profile/cancellation-success.tsx
+++ src/Pages/profile/cancellation-success.tsx
@@ -1,19 +1,38 @@
 import { ReactComponent as CancellationSuccessTick } from "../../images/utils/cancellation-success-tick.svg";
+import { useEffect, useState } from "react";    
+import { navigate } from "@reach/router";
 interface CancellationSuccessProps {
     isOpen: boolean;
     onClose: () => void;
 }
 
 const CancellationSuccess: React.FC<CancellationSuccessProps> = ({ isOpen, onClose }) => {
+    const [countdown, setCountdown] = useState(3);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if(countdown > 1) {
+                setCountdown(countdown - 1);
+            } else {
+                onClose();
+                window.location.reload();
+            }
+        }, 1000);
+        return () => clearInterval(interval);
+    }, [countdown]);
+
     return (
         <div className="flex flex-col gap-4 px-4">
             <div className="flex flex-row justify-center items-center w-1/2 mx-auto">
                 <CancellationSuccessTick />
             </div>
-            <div className="text-sm font-normal text-black">Your booking has been cancelled successfully</div>
+            <div className="w-full text-sm font-bold text-black text-center">Your booking has been cancelled successfully</div>
             <div className="flex flex-row justify-between gap-5 mb-6">
-                <div className="w-full bg-yellow-300 rounded-lg px-4 py-3" onClick={onClose}>
+                {/* <div className="w-full bg-yellow-300 rounded-lg px-4 py-3" onClick={onClose}>
                     <div className="flex flex-row justify-center text-black text-sm font-bold">Done</div>
+                </div> */}
+                <div className="w-full px-4 py-3 text-xs font-normal text-black text-center" >
+                    Redirecting to profile page in {countdown} seconds...
                 </div>
             </div>
         </div>
