57ed7644ec0642008557e10d61858f57ecc41467
pratik-thorat-github <pratik.thorat01@gmail.com>
2025-05-28 10:00:40 +0530

    changed cancellation policy

--- src/Pages/profile/refund-policy.tsx
+++ src/Pages/profile/refund-policy.tsx
@@ -8,25 +8,29 @@ const RefundPolicy: React.FC<{booking: IBookings}> = ({ booking }) => {
 
         const refundPolicyObject = JSON.parse(refundPolicy);
 
-        if(!refundPolicy) {
-            points.push("There is no refund policy for this booking");
+
+
+        if(!refundPolicy || refundPolicy == "{}") {
+            points.push("❌ There is no refund policy for this booking");
             return points;
         }
 
         if(refundPolicyObject.refund_applicable === false) {
-            points.push("Refund is not applicable for this booking");
+            points.push("❌ Refund is not applicable for this booking");
             return points;
         }
 
         if(refundPolicyObject.conditions?.length > 0) {
-            points.push("Post Cancellation, it will take 5-7 working days to get refund credited to your source account.");
             // headers.push("Refund conditions are as follows:");
             for(let i = refundPolicyObject.conditions.length - 1; i >= 0; i--) {
                 const condition = refundPolicyObject.conditions[i];
                 if(condition.refund_percentage == 100) {
-                    points.push(`Full refund will be made if the booking is cancelled before ${condition.minutes_before / 60} hours.`);
+                    points.push(`💯 Full refund if cancelled at least ${condition.minutes_before / 60} hours before the start time.`);
                 } else {
-                    points.push(`${condition.refund_percentage}% of the total booking amount will be refunded if the booking is cancelled before ${condition.minutes_before / 60} hours.`);
+                    points.push(`🔁 ${condition.refund_percentage}% refund if cancelled at least ${condition.minutes_before / 60} hours before the start time.`);
+                }
+                if(i == 0) {
+                    points.push(`❌ No refund if cancelled less than ${condition.minutes_before / 60} hours before the start time.`);
                 }
             }
         }
@@ -37,12 +41,27 @@ const RefundPolicy: React.FC<{booking: IBookings}> = ({ booking }) => {
 
 
     return (
-        <div className="flex flex-col text-sm py-2 text-gray-500">
-            <ul>
-                {refundPolicy.map((point: string, index: number) => (
-                    <li key={index} className="list-disc list-inside">{point}</li>
-                ))}
-            </ul>
+        refundPolicy.length == 1 ?
+        <div className="flex flex-col text-sm pt-4 text-gray-500">
+            {refundPolicy[0]}
+        </div> :
+        <div className="flex flex-col">
+            <div className="flex flex-col text-sm font-bold py-2 text-gray-500">
+                💸 Refund Timeline:
+            </div>
+            <div className="flex flex-col text-sm text-gray-500 px-4 pb-2">
+                <p>Once cancelled, your refund will be credited to the original payment method within <b>5–7 working days.</b></p>
+            </div>
+            <div className="flex flex-col font-bold text-sm text-gray-500">
+                <p>💰 How much you’ll get back:</p>
+            </div>
+            <div className="flex flex-col text-sm text-gray-500 px-2 pb-2">
+                <ul className="list-disc list-inside text-sm">
+                    {refundPolicy.map((point: string, index: number) => (
+                        <li key={index}>{point}</li>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 }
