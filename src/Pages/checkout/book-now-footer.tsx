0034f09ff5157bcb6c0de2e72a3f881169f26032
pratik-thorat-github <pratik.thorat01@gmail.com>
2025-06-05 12:38:53 +0530

    coins used change

--- src/Pages/checkout/book-now-footer.tsx
+++ src/Pages/checkout/book-now-footer.tsx
@@ -89,7 +89,7 @@ function createOrderPayload(props: IBookNowFooter, userDetails: IUser) {
     batchTime: props?.batchDetails?.startTime || 0,
     participants: props?.batchDetails?.participants || [],
     coins: props.coinsUsed ? props.coinsUsed : props.coinsAvailable || 0,
-    coinsUsed: props.coinsUsed && props.coinsAvailable ? props.totalAmount <= props.coinsAvailable ? props.totalAmount : props.totalAmount - props.coinsAvailable : 0,
+    coinsUsed: props.coinsUsed && props.coinsAvailable ? props.totalAmount <= props.coinsAvailable ? props.totalAmount : props.coinsAvailable : 0,
   };
 
   Mixpanel.track("pay_now_button_clicked_on_checkout_page", {
