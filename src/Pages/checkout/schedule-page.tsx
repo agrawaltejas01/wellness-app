b22f70679d6521764af4d49ecc4584eb03aa61ea
pratik-thorat-github <pratik.thorat01@gmail.com>
2025-06-17 11:27:00 +0530

    date fix in weekend batch

--- src/Pages/checkout/schedule-page.tsx
+++ src/Pages/checkout/schedule-page.tsx
@@ -769,7 +769,7 @@ const SchedulePage: React.FC<IClassCheckout> = ({}) => {
                 _getGymBatchesForDate({
                   id: gym.gymId,
                   activity: activity,
-                  date: "2025-06-16",
+                  date: "2025-06-17",
                 });
               }}
               reposition
