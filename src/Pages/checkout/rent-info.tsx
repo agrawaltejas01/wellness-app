be31f237492f730e381179036700e060d1b1d89c
pratik-thorat-github <pratik.thorat01@gmail.com>
2025-06-24 14:58:23 +0530

    fixed retal copy

--- src/Pages/checkout/rent-info.tsx
+++ src/Pages/checkout/rent-info.tsx
@@ -1,9 +1,18 @@
 const RentInfo: React.FC<{}> = ({}) => {
     return (
-        <div className="flex flex-col gap-2 pt-2">
-            <p>🏸 A fresh Mavis 350 shuttle will be given (rental charges apply).</p>
-            <p>Joining an Open Play? Renting the shuttle is mandatory.</p>
-            <p>Booking a full court? You can choose to rent a shuttle or bring your own!</p>
+        <div className="flex flex-col gap-2 pt-2 text-sm">
+            <div className="flex flex-col">
+                <p>🏸 A fresh Mavis 350 shuttle will be given </p>
+                <p className="font-bold text-xs">(rental charges applicable).</p>
+            </div>  
+            <div className="flex flex-col"> 
+                <p>Joining an Open Play? </p>
+                <p className="text-gray font-light text-xs">Renting the shuttle is mandatory.</p>
+            </div>
+            <div className="flex flex-col">
+                <p>Booking a full court? </p>
+                <p className="text-gray font-light text-xs">You can choose to rent a shuttle or bring your own!</p>
+            </div>
         </div>
     )
 }
