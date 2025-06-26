5f819910f3b510d60f379a58288148f45cd5bffc
pratik-thorat-github <pratik.thorat01@gmail.com>
2025-06-27 01:28:29 +0530

    removed swimming strength from popular activities

--- src/Pages/home/classes-near-you.tsx
+++ src/Pages/home/classes-near-you.tsx
@@ -91,9 +91,11 @@ function createUpperFlexTiles(isFromApp: boolean, pastAppBookings: PastAppBookin
       <span> {classTile("badmintonKids", isFromApp, pastAppBookings)} </span>
       {/* <span> {classTile("ride", isFromApp, pastAppBookings)} </span> */}
       <span>{classTile("badminton", isFromApp, pastAppBookings)}</span>
-      <span>{classTile("strength", isFromApp, pastAppBookings)}</span>
-      <span>{classTile("swimming", isFromApp, pastAppBookings)}</span>
-      <span>{classTile("personaltraining", isFromApp, pastAppBookings)}</span>
+      {/* <span>{classTile("strength", isFromApp, pastAppBookings)}</span> */}
+      {/* <span>{classTile("swimming", isFromApp, pastAppBookings)}</span> */}
+      {/* <span>{classTile("personaltraining", isFromApp, pastAppBookings)}</span> */}
+      <span> {classTile("gymming", isFromApp, pastAppBookings)} </span>
+      <span> {classTile("boxing", isFromApp, pastAppBookings)} </span> 
     </Flex>
   );
 }
@@ -113,8 +115,8 @@ function createLowerFlexTiles(isFromApp: boolean, pastAppBookings: PastAppBookin
       <span> {classTile("football", isFromApp, pastAppBookings)} </span>
       <span> {classTile("gym", isFromApp, pastAppBookings)} </span>
       <span> {classTile("zumba", isFromApp, pastAppBookings)} </span>
-      <span> {classTile("boxing", isFromApp, pastAppBookings)} </span>
-      <span> {classTile("gymming", isFromApp, pastAppBookings)} </span>
+      {/* <span> {classTile("boxing", isFromApp, pastAppBookings)} </span>  */}
+      {/* <span> {classTile("gymming", isFromApp, pastAppBookings)} </span> */}
     </Flex>
   );
 }
