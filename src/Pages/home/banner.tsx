e48988cf92bbd30757934801bc5c1b13e8ea5a80
pratik-thorat-github <pratik.thorat01@gmail.com>
2025-07-11 00:17:53 +0530

    added coins

--- src/Pages/home/banner.tsx
+++ src/Pages/home/banner.tsx
@@ -9,12 +9,33 @@ import { useAtom } from "jotai/react";
 import { plusDetailsAtom } from "../../atoms/atom";
 import PlusClassRemaining from "../profile/plus-classes-remaining";
 import { ReactComponent as Banner } from "../../images/home/banner.svg";
+import CoinsHomepage from "../coins/coins-homepage";
+import { getCoins } from "../../apis/coins/coins";
+import { useMutation } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import Places from "./places";
+
+
 
 
 const HomeBanner: React.FC = () => {
   const [plusDetails] = useAtom(plusDetailsAtom);
+  const [coins, setCoins] = useState(0);
 
   const userDetails =window.localStorage.getItem("zenfitx-user-details") && JSON.parse(window.localStorage["zenfitx-user-details"])
+
+  const { mutate: _getCoins } = useMutation({
+    mutationFn: getCoins,
+    onSuccess: (result) => {
+        setCoins(result.coins);
+    }
+  });
+
+  useEffect(() => {
+    _getCoins(userDetails?.id);
+  }, []);
+
+
   return (
     // <Flex
     //   onClick={() => {
@@ -74,7 +95,9 @@ const HomeBanner: React.FC = () => {
     //   </Flex> */}
     // </Flex>
     <>
-    <div style={{'width':'100%'}}><Banner/></div>
+    {/* <div style={{'width':'100%'}}><Banner/></div> */}
+    {/* <div style={{'width':'100%'}}>Hello</div> */}
+    <div style={{'width':'100%', 'backgroundColor':'black' }}>
     {!userDetails && <div className="homeWrapper">
       <div style={{paddingTop:"40px"}}>
         <span className="zenfit">Zenfit</span><span className="x">X</span>
@@ -90,17 +113,38 @@ const HomeBanner: React.FC = () => {
       </div>
     </div>}
     {userDetails && <div className="loginWrapper">
-      <div className="loginHead1">
-        <span className="loginName">Hi {userDetails?.name.split(' ')[0]}</span><span className="bookings" onClick={()=>navigate('/profile')}>Bookings</span>
+      <div className="loginHead1 justify-between flex flex-row">
+        <Places />
+        <div className="flex flex-row gap-3 w-full justify-end">
+          {coins > 0 && <div className="flex flex-row rounded-full gap-2 items-center border border-white p-1 bg-red-500 bg-opacity-50" 
+              onClick={()=>{
+                navigate('/coins')
+                Mixpanel.track("clicked_coins_capsule_home", {
+                  user_id: userDetails?.id
+                });
+              }}>
+            <img src={require('../../images/home/coin.jpg')} className="rounded-lg w-4 h-4"/>
+            <p className="text-white text-sm font-bold">{coins}</p>
+          </div>}
+          <div className="flex text-white text-sm border rounded-lg px-2 py-1" onClick={()=>navigate('/profile')}>Bookings</div>
+        </div>
 
       </div>
-      <div className="loginHead2">
-      Welcome to ZenfitX!
+      <div className="loginName">
+          Hi {userDetails?.name.split(' ')[0]}
+      {/* Welcome to ZenfitX! */}
+          {/* EXPLORE & BOOK */}
       </div>
       <div className="loginHead3">
       Explore & book any fitness or wellness activity near you!
+        {/* Try different fitness activities in your area with us! */}
+      </div>
+        
+      <div className="w-full mt-6">
+        <CoinsHomepage />
       </div>
     </div>}
+    </div>
     </>
   );
 };
