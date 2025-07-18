dbbc0615f42942333da4a104f3c8344a21f6c99a
pratik-thorat-github <pratik.thorat01@gmail.com>
2025-07-18 19:15:47 +0530

    minor fix

--- src/hooks/useMixpanelTracking.ts
+++ src/hooks/useMixpanelTracking.ts
@@ -5,22 +5,26 @@ export const useMixpanelTracking = () => {
   useEffect(() => {
     const handleMessage = (event: any) => {
       const message = event.data;
-      const type = JSON.parse(message).type;
-      const timestamp = JSON.parse(message).timestamp;
-      const data = JSON.parse(message).data;
-      const source = JSON.parse(message).source;
 
-      Mixpanel.track(`${type}`, {
-        message: message,
-        timestamp: timestamp,
-        data: data,
-        source: source
-      });
+      alert(message);
     };
+
+    //   const type = JSON.parse(message).type;
+    //   const timestamp = JSON.parse(message).timestamp;
+    //   const data = JSON.parse(message).data;
+    //   const source = JSON.parse(message).source;
+
+    //   Mixpanel.track(`${type}`, {
+    //     message: message,
+    //     timestamp: timestamp,
+    //     data: data,
+    //     source: source
+    //   });
+    // };
 
     window.addEventListener("message", handleMessage);
 
-    // Cleanup function to remove event listener
+    // // Cleanup function to remove event listener
     return () => {
       window.removeEventListener("message", handleMessage);
     };
