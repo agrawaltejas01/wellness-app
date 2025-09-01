6361e3909a30b6b0fcc6ff396c18d2bf02d0c2f2
pratik-thorat-github <pratik.thorat01@gmail.com>
2025-09-01 14:47:07 +0530

    changed z index for back button

--- src/Pages/highlights/a.tsx
+++ src/Pages/highlights/a.tsx
@@ -349,7 +349,7 @@ const ReelsVideoPlayer: React.FC<ReelsVideoPlayerProps> = ({ src, caption }) => 
       </div>
 
       {/* Highlight Text at Top-Left Corner */}
-      <div className="absolute top-4 left-4" onClick={() => navigate('/', {replace: true})}>
+      <div className="absolute top-4 left-4 z-20" onClick={() => navigate('/', {replace: true})}>
         <span className="font-semibold text-lg text-white"><BackButton /></span>
       </div>
 
