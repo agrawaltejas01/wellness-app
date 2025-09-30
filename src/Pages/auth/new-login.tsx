dad3423cd4f6b9ee559308cf55954afa92290081
pratik-thorat-github <pratik.thorat01@gmail.com>
2025-09-30 11:17:51 +0530

    phone number fix

--- src/Pages/auth/new-login.tsx
+++ src/Pages/auth/new-login.tsx
@@ -53,7 +53,7 @@ const NewLogin: React.FC<INewLoginProps> = () => {
     if (phoneNumber.length === 10 && !submitClicked.current) {
       submitClicked.current = true;
       setIsLoading(true);
-      sendOtp({ phone: phoneNumber });
+      sendOtp(phoneNumber);
     }
   };
 
