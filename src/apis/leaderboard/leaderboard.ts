23d7a3adae81a86a49794a303e4558bd3902580b
pratik-thorat-github <pratik.thorat01@gmail.com>
2025-10-01 18:51:48 +0530

    added data in leaderboard from api

--- src/apis/leaderboard/leaderboard.ts
+++ src/apis/leaderboard/leaderboard.ts
@@ -4,3 +4,8 @@ export async function getTop3Players(activityId: number) {
   const result = await networkAdapter.get(`/leaderboard/top-players?activity_id=${activityId}`);
   return result.data;
 }
+
+export async function getLeaderboard({activityId, pageSize, pageNumber}: {activityId: number, pageSize: number, pageNumber: number}) {
+  const result = await networkAdapter.get(`/leaderboard/activity?activity_id=${activityId}&pageSize=${pageSize}&pageNumber=${pageNumber}`);
+  return result.data;
+}
