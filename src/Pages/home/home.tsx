6c94b323dd13511b441345f7a65e4a94908ee10e
pratik-thorat-github <pratik.thorat01@gmail.com>
2025-10-01 17:52:03 +0530

    added top 3 players api

--- src/Pages/home/home.tsx
+++ src/Pages/home/home.tsx
@@ -244,6 +244,7 @@ const Home: React.FC<IHome> = ({ activitySelected, showClassesNearYou }) => {
     }
   }, [userDetails]);
 
+
   function setCookie(name: string, value: string, days: number) {
     const date = new Date();
     date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000); // Convert days to milliseconds
@@ -291,7 +292,7 @@ const Home: React.FC<IHome> = ({ activitySelected, showClassesNearYou }) => {
             <HomeBannerV2 userDetails={userDetails as IUser} />
           </Flex>
 
-          <LeaderboardHome />
+          <LeaderboardHome activityId={1} />
           <div className="w-full mt-2 px-4">
             <Highlights />
           </div>
