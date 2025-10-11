e84b7194860739749def6d4f40a2cca3221eb8af
pratik-thorat-github <pratik.thorat01@gmail.com>
2025-10-11 11:50:24 +0530

    rating home page changes

--- src/Pages/home/rating-homepage.tsx
+++ src/Pages/home/rating-homepage.tsx
@@ -13,11 +13,13 @@ import { CenterModal } from "../profile/center-modal";
 
 
 const NoRating = ({games, isLoadingGames}: {games: number, isLoadingGames: boolean}) => {
+    const [isRatingInfoModalOpen, setIsRatingInfoModalOpen] = useState(false);
+
     return (
         <div className="flex flex-col gap-2 items-center justify-center mt-2">
             <Growth />
             <div className="flex flex-row gap-2 rounded-full bg-white p-2">
-                <div className="flex flex-col text-black text-xs pl-4 pr-2 items-center">
+                <div className="flex flex-col text-black text-xs pl-4 pr-2 items-center" onClick={()=>setIsRatingInfoModalOpen(true)}>
                     <div className="text-2xl">🔒</div>
                     <div className="font-extralight">Rating</div>
                 </div> 
@@ -29,20 +31,27 @@ const NoRating = ({games, isLoadingGames}: {games: number, isLoadingGames: boole
             </div>
             <div className="flex flex-col items-center"> 
                 <div className="flex flex-row" >
-                    <span className="font-bold text-base font-sans">Get your SESH analysis</span>
+                    <span className="font-bold text-base">Unlock your Zen Rating now!</span>
+                    <InfoCircleOutlined className="w-4 h-4 self-center ml-1" onClick={()=>setIsRatingInfoModalOpen(true)}/>
                 </div>  
                 <div className="flex flex-row ">
-                    <span className="font-extralight text-sm">Learn & improve your game with us</span>
+                    <span className="font-extralight text-sm">Track, Improve & Play smartly with ZenVision AI</span>
                 </div>
             </div>
             <div className="w-full">
-                <div className="rounded-full p-2 w-full items-center justify-center text-white"style={{'background': '#009605'}}  onClick={()=>navigate('/badminton')}>
+                <div className="rounded-full mt-2 p-2 w-full items-center justify-center text-black"style={{'background': 'white'}}  onClick={()=>navigate('/badminton')}>
                     <div className="flex flex-row justify-center gap-1">
-                        <PlusWhite />
+                        <Plus />
                         Book New Game
                     </div>
                 </div>
             </div>  
+            <CenterModal
+                isOpen={isRatingInfoModalOpen}
+                onClose={()=>setIsRatingInfoModalOpen(false)}
+                title="Rating - Quick Guide"
+                children={<RatingInfo />}
+            />
         </div>
     )
 }
@@ -53,26 +62,26 @@ const Rating = ({rating, games, isLoadingRating, isLoadingGames}: {rating: numbe
     return (
         <div className="flex flex-col gap-4 items-center justify-center mt-4">
             <div className="flex flex-row gap-4">
-                <div className={`flex flex-col text-black text-xs pl-4 pr-2 items-center ${isLoadingRating ? 'animate-pulse-slow' : ''}`}>
+                <div className={`flex flex-col w-1/2 text-black text-xs pl-4 pr-2 items-center ${isLoadingRating ? 'animate-pulse-slow' : ''}`} onClick={()=>setIsRatingInfoModalOpen(true)}>
                     <div className="flex flex-row gap-1">
-                        <div className="text-6xl">{isLoadingRating ? '...' : rating/100}</div>
-                        {/* <div className="text-2xl self-center">/10</div> */}
+                        <div className="text-5xl">{isLoadingRating ? '...' : rating/100}</div>
+                        <div className="text-2xl self-end">/10</div>
                     </div>
                     <div className="flex flex-row gap-1">
                     <div className="font-extralight">
                         Rating
                     </div>
-                        <InfoCircleOutlined className="w-3 h-3 self-center" onClick={()=>setIsRatingInfoModalOpen(true)} />
+                        <InfoCircleOutlined className="w-3 h-3 self-center" />
                     </div>
                 </div> 
                 <div className="border-r border-gray" />
-                <div className={`flex flex-col text-black text-xs pr-4 pl-2 items-center ${isLoadingGames ? 'animate-pulse-slow' : ''}`}>
-                    <div className="text-6xl">{isLoadingGames ? '...' : games}</div>
+                <div className={`w-1/2 flex flex-col text-black text-xs pr-4 pl-2 items-center ${isLoadingGames ? 'animate-pulse-slow' : ''}`}>
+                    <div className="text-5xl">{isLoadingGames ? '...' : games}</div>
                     <div className="font-extralight">Games</div>
                 </div>
             </div>
             <div className="w-full" onClick={()=>navigate('/badminton')}>
-                <div className="rounded-full p-2 w-full items-center justify-center bg-white text-black border border-black border-dashed">
+                <div className="rounded-full p-2 mt-2 w-full items-center justify-center bg-white text-black border border-black border-dashed">
                     <div className="flex flex-row justify-center gap-1 p-1">
                         <Plus />
                         <span className="font-light">Book New Game</span>
@@ -129,7 +138,7 @@ const RatingHomepage: React.FC<{userDetails: IUser}> = ({userDetails}) => {
     const isLoading = isLoadingRating || isLoadingGames;
 
     return (
-        <div className={`mx-4 rounded-3xl shadow-lg p-2 border border-white bg-white ${isLoading ? 'animate-pulse-slow' : ''}`}>
+        <div className={`rounded-3xl shadow-[0_0_15px_rgba(0,0,0,0.2)] p-2 ${isLoading ? 'animate-pulse-slow' : ''}`} style={{'background': 'linear-gradient(to right, rgba(199, 255, 202, 1), rgba(238, 255, 183, 1))'}}>
             {!isLoadingRating && rating === 0 ? <NoRating games={games} isLoadingGames={isLoadingGames} /> : <Rating rating={rating} games={games} isLoadingRating={isLoadingRating} isLoadingGames={isLoadingGames} />}
         </div>
     )
