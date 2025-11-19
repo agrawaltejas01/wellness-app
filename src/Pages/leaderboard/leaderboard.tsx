228edfc5478d55653ce160e61ef2949ec6a41e4a
pratik-thorat-github <pratik.thorat01@gmail.com>
2025-11-19 13:20:20 +0530

    rating last updated changed

--- src/Pages/leaderboard/leaderboard.tsx
+++ src/Pages/leaderboard/leaderboard.tsx
@@ -269,7 +269,7 @@ const Leaderboard = (props: LeaderboardProps) => {
                         </button>
                         <div className="flex flex-col">
                         <h1 className="text-2xl font-bold  text-gray-900">Leaderboard 🏆</h1>
-                        <span className="text-xs text-gray-500 font-normal">Last updated: Oct 18th, 4 PM</span>
+                        <span className="text-xs text-gray-500 font-normal">Last updated: Nov 19th, 12 PM</span>
                         </div>
                     </div>
                 </div>
