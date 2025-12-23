3ff1435eb9b910aa0b6bce5180264151ebbd76b6
pratik-thorat-github <pratik.thorat01@gmail.com>
2025-12-23 10:54:56 +0530

    date

--- src/Pages/checkout/schedule-page.tsx
+++ src/Pages/checkout/schedule-page.tsx
@@ -813,7 +813,7 @@ const SchedulePage: React.FC<IClassCheckout> = ({}) => {
                 _getGymBatchesForDate({
                   id: gym.gymId,
                   activity: activity,
-                  date: "2025-12-23",
+                  date: "2025-12-24",
                 });
               }}
               reposition
