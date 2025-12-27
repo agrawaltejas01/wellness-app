5fc1e69ebb68cb218d8910688415b065f7184876
pratik-thorat-github <pratik.thorat01@gmail.com>
2025-12-27 15:29:42 +0530

    rating info

--- src/utils/rating-badge-homescreen.tsx
+++ src/utils/rating-badge-homescreen.tsx
@@ -1,4 +1,7 @@
 import { InfoCircleOutlined } from "@ant-design/icons";
+import { useState } from "react";
+import RatingInfo from "../Pages/home/rating-info";
+import { CenterModal } from "../Pages/profile/center-modal";
 
 const getBackgroundColor = (rating: number) => {
     if(rating > 0 && rating < 250) return { backgroundColor: 'rgba(107, 227, 156, 0.15)', fontColor: '#6BE39C' };
@@ -11,16 +14,23 @@ const getBackgroundColor = (rating: number) => {
 }
 
 export const RatingBadgeHomeScreen: React.FC<{ rating: number}> = ({ rating }) => {
+    const [isRatingInfoModalOpen, setIsRatingInfoModalOpen] = useState(false);
     return (
         <div className="flex flex-row items-center gap-1 rounded-full py-2 justify-between pl-4 px-3" style={{backgroundColor: getBackgroundColor(rating).backgroundColor}}>
             <div className="flex flex-col text-black" style={{ fontFamily: "Plus Jakarta Sans, sans-serif", fontSize: '10px',letterSpacing: '0.15em' }}>
-                <span className="font-bold text-xs">ZBR <InfoCircleOutlined className="w-2 h-2 self-center" /> </span> 
+                <span className="font-bold text-xs">ZBR <InfoCircleOutlined className="w-2 h-2 self-center" onClick={()=>setIsRatingInfoModalOpen(true)} /> </span> 
                 <span className="text-light">RATING</span>
             </div>
             <div className="px-2 py-1 rounded-full text-white font-bold" style={{backgroundColor: getBackgroundColor(rating).fontColor, fontSize: '16px'}}>
               {(rating / 100)} ⭐️ 
             </div>
-            
+            <CenterModal
+                isOpen={isRatingInfoModalOpen}
+                onClose={()=>setIsRatingInfoModalOpen(false)}
+                title="Rating (ZBR) - Quick Guide"
+                subtitle="ZenfitX Badminton Rating"
+                children={<RatingInfo />}
+            />
         </div>
     )
 }
